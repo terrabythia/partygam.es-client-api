c2bdda80b196c8470df94819b46b0b3376f4b3d4
Sander Bruggeman <sander@air-van-sander.home>
2020-04-29 21:23:59 +0200

    Setup for NPM publishing, moved library to single file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,88 @@
-import {
-    IPlayer,
-    PartyTimeIncomingMessage,
-    PartyTimeIncomingMessageType,
-    PartyTimeOutgoingMessage,
-    PartyTimeOutgoingMessageType,
-    sProtocolName
-} from "./protocol";
+export interface IPlayer {
+    id: string;
+    assignedColor?: string;
+    isCurrent: boolean;
+    isHost: boolean;
+    username: string;
+    joinedAtTimestamp: number;
+}
+
+type protocolName = 'partygam.es';
+export const sProtocolName = 'partygam.es';
+
+export enum PartyTimeIncomingMessageType {
+    IFRAME_IS_READY,
+    MESSAGE,
+    SHOW_TOAST,
+    PLAYERS_DID_UPDATE,
+}
+
+export enum PartyTimeOutgoingMessageType {
+    BROADCAST_MESSAGE = 4,
+    EMIT_MESSAGE,
+    DIRECT_MESSAGE,
+    REQUEST_PLAYERS_UPDATE,
+    SHOW_GENERAL_TOAST,
+    SHOW_PLAYER_TOAST,
+    SET_IFRAME_HEIGHT,
+}
+
+export enum PartyTimeInternalMessageType {
+    HANDSHAKE = 11,
+    CONNECTIONS_DID_UPDATE ,
+}
+
+export interface PartyTimeIncomingMessage {
+    source: protocolName,
+    type: PartyTimeIncomingMessageType,
+    playerId?: string;
+    payload?: any;
+}
+
+export interface PartyTimeOutgoingMessage {
+    source: protocolName;
+    type: PartyTimeOutgoingMessageType;
+    playerId?: string;
+    payload?: any;
+}
+
+export interface PartyTimeInternalMessage {
+    source: protocolName;
+    type: PartyTimeInternalMessageType;
+    payload?: any;
+}
 
 type OnMessageReceivedListener = (playerId: string, payload: any) => void;
-type OnPlayersUpdateListener = (players: ReadonlyArray<IPlayer>) => void;
+type OnPlayersUpdateListener = ({ players }: { players: ReadonlyArray<IPlayer> }) => void;
 
+let iframeIsReady: boolean = false;
+let messageQueue: ReadonlyArray<PartyTimeOutgoingMessage> = [];
 let messageReceivedListeners: ReadonlyArray<OnMessageReceivedListener> = [];
 let playersUpdateListeners: ReadonlyArray<OnPlayersUpdateListener> = [];
+
+const handleQueue = () => {
+    if (!iframeIsReady) {
+        return;
+    }
+    console.log({messageQueue});
+    // TODO: maybe send all at once?
+    messageQueue.forEach((message) => {
+        window.parent.postMessage(
+            message,
+            '*'
+        );
+    });
+    messageQueue = [];
+};
+
+// TODO: add possibility to send multiple messages at once (array of messages)?
+// TODO: on start set a timeout with a queue while we wait
+const messageParent = (message: PartyTimeOutgoingMessage) => {
+    messageQueue = messageQueue.concat(
+        message
+    );
+    handleQueue();
+};
 
 export const onPlayersUpdate = (listener: OnPlayersUpdateListener, { immediate = false }) => {
     playersUpdateListeners = playersUpdateListeners.concat(listener);
@@ -27,12 +98,19 @@ export const onPlayersUpdate = (listener: OnPlayersUpdateListener, { immediate =
     };
 };
 
-// TODO: add possibility to send multiple messages at once (array of messages)?
-const messageParent = (message: PartyTimeOutgoingMessage) => {
-    window.parent.postMessage(
-        message,
-        '*'
-    );
+/**
+ * Update the game's iframe height. Between screen filling and 200vh.
+ * @param height
+ */
+export const updateGameHeight = (height?: number) => {
+    console.log('send message nr', PartyTimeOutgoingMessageType.SET_IFRAME_HEIGHT);
+    messageParent({
+        source: sProtocolName,
+        type: PartyTimeOutgoingMessageType.SET_IFRAME_HEIGHT,
+        payload: {
+            height,
+        }
+    });
 };
 
 export const broadcastMessage = (payload: any) => {
@@ -100,6 +178,12 @@ window.addEventListener('message', (event) => {
     }
 
     console.log('IFRAME got message', event);
+
+    if (type === PartyTimeIncomingMessageType.IFRAME_IS_READY) {
+        iframeIsReady = true;
+        handleQueue();
+        return;
+    }
 
     if (type === PartyTimeIncomingMessageType.PLAYERS_DID_UPDATE) {
         playersUpdateListeners.forEach((listener) => listener(
